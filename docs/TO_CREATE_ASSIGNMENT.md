dbd3dd89b250de3c2b18e2a2c5bca2d089eb53d9
petecarapetyan <pete@datafundamentals.com>
2021-10-28 11:58:56 -0500

    WIP

---
+++ docs/TO_CREATE_ASSIGNMENT.md
@@ -0,0 +1,10 @@
+# To Create an Assignment
+
+- Use this template to create a new project named AssignmentN, where N is 1-14
+- Remove all but these from the docs folder
+ - - JUNIT_REF
+ - - PROBLEMS_TO_EXPECT
+ - - WHO_HELPED
+- Fix README.md entirely
+- remame the project in the pom.xml file to `assignmentN`
+- 
--- src/main/java/com/coderscampus/Main.java
+++ src/main/java/com/coderscampus/Main.java
@@ -3,8 +3,17 @@ package com.coderscampus;
 public class Main {
 
   /**
+   * PLEASE DO NOT PUT ANY CODE IN THIS CLASS FOR THIS ASSIGNMENT
+   * 
    * To avoid confusing students with a static method that can only call static methods, 
-   * this class only instantiates the App and, then launches it.
+   * this class only instantiates the App and, then launches that.
+   * 
+   * All the code which might otherwise go in this main method would then
+   * go inside the App.launch() method
+   * 
+   * Static classes, methods, and fields are all wonderful things when you
+   * know how to avoid the foot-guns associated with statics. But until
+   * then, they are perhaps best avoided.
   */
   public static void main(String[] args){
     new App().launch();
